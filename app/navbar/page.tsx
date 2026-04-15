52f768fdcc3f260dff27c7daaca20d2391df7fe3
Ayanbajo Funsho <fayanbajo@gmail.com>
2026-04-15 14:49:15 +0100

    updated -v

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -34,9 +34,9 @@ export default function Navbar() {
               Home
             </Link>
 
-            <Link href="/about" className="font-sans sm:text-xs md:text-xs lg:text-sm font-bold px-3 py-2 rounded-lg border">
+            {/* <Link href="/about" className="font-sans sm:text-xs md:text-xs lg:text-sm font-bold px-3 py-2 rounded-lg border">
               About
-            </Link>
+            </Link> */}
 
             <Link href="/features" className="font-sans sm:text-xs md:text-xs lg:text-sm font-bold px-3 py-2 rounded-lg border">
               Features
@@ -87,7 +87,7 @@ export default function Navbar() {
             </button>
 
             <Link href="/" onClick={() => setOpen(false)}>Home</Link>
-            <Link href="/about" onClick={() => setOpen(false)}>About</Link>
+            {/* <Link href="/about" onClick={() => setOpen(false)}>About</Link> */}
             <Link href="/features" onClick={() => setOpen(false)}>Features</Link>
             <Link href="/how-it-works" onClick={() => setOpen(false)}>How it works</Link>
             <Link href="/login" onClick={() => setOpen(false)}>Login</Link>
